a89ba89ae5b97f40d1b2be9bdb54e6fc1511151b
essz4367 <szantho.eszti@gmail.com>
2023-02-18 11:09:10 +0100

    poker_types

---
+++ poker_types.ts
@@ -0,0 +1,24 @@
+import {type List, type Pair} from '../lib/list'
+
+export type Permutation = Array<number>;
+
+export type Card = {
+    suit: number,
+    value: number, 
+    name?: string
+};
+
+export type Deck = List<Card>;
+
+export type Board = Array<Card>;
+
+export type Hand = Array<Card>;
+
+export type Pocketcards = Array<Card>;
+
+export type Pokerhand = {
+    exists: Boolean
+    value?: number 
+    rang?: number
+    name?: string
+}
