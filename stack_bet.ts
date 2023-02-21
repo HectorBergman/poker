cfcdec92f14dddae36631bed7d75b4824eaea390
essz4367 <szantho.eszti@gmail.com>
2023-02-21 11:00:34 +0100

    function specifications and eslint

--- stack_bet.ts
+++ stack_bet.ts
@@ -1,5 +1,5 @@
-import {Hand, Card, Pokerhand, Chip, Bet, Pot, Stack, Pile} from './poker_types'
-import * as PromptSync from "prompt-sync";
+import {Bet, Pot, Stack, Pile} from './poker_types';
+//import * as PromptSync from "prompt-sync";
 import {tail, head} from '../lib/list';
 
 /**
@@ -63,14 +63,56 @@ function remove_from_stack(stack: Stack, bet_pile: Pile): Stack {
     return stack;
 }
 
+/**
+ * Makes a new empty pot
+ * @returns 
+ */
+function make_pot(): Pot {
+    return [ make_pile(white, 1, 0),
+             make_pile(red, 5, 0),
+             make_pile(blue, 10, 0),
+             make_pile(green, 25, 0), ];
+}
 
-//function add_to_pot(pile: Pile, pot: Pot): Pot {}
+/**
+ * Adds a given bet pile to pot
+ * @param pot (array) an array which marks the places for chip piles according to color
+ * @param bet_pile (type record) a parsed bet to be added to the pot
+ * @returns a pot with the given bet added
+ */
+function add_bet(pot: Pot, bet_pile: Pile): Pot {
+    const color = bet_pile.color;
+    pot[color].number = bet_pile.number + pot[color].number; 
+    return pot;
+} 
+
+/**
+ * Counts what a given pot is worth
+ * @param pot (Array) a Pot with Piles of chips with different values and sizes
+ * @returns the value of the pot in dollar
+ */
+function pot_value(pot: Pot): number {
+    let value = 0;
+    for (let i = 0; i <= 3; i += 1) {
+        value = value + pot[i].chip * pot[i].number;
+    }
+    return value;
+}
+
+//function fair_pot
+
 
 const stack1: Stack = make_new_stack();
 const bet = parse_bet(['white', 3]);
+const pot = make_pot();
 //const pile1: Pile = make_pile(3)
-remove_from_stack(stack1, bet)
+remove_from_stack(stack1, bet);
 console.log(bet);
 console.log(stack1);
+
+const pot2 = add_bet(pot, bet);
+console.log(pot2);
+console.log(pot_value(pot2));
+
 
 
