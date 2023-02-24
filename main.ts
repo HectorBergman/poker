508289bea6867b2eea6f55b2538a2c06dc74f75a
essz4367 <szantho.eszti@gmail.com>
2023-02-24 11:20:27 +0100

    main

--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
 //main file
 import {holdem} from "./poker";
 import {Pot, GameState} from "./poker_types";
-import {make_pot, make_new_stack, show_game_state} from "./stack_bet"
+import {make_pot, make_new_stack, show_game_state, add_pot} from "./stack_bet"
+import {} from "./poker_hands"
 
-function round() {
+function round(gs: GameState) {
     let pot1 = make_pot();
     let pot2 = make_pot();
-    return holdem(2);
+    const hands: Array<Hand> | undefined = holdem(2, pot1, pot2, gs);
+    if (hands == undefined) {
+        add_pot(pot1, gs[1]);
+        add_pot(pot2, gs[1])
+
+    } else {
+
+    }
+
+
 }
 
 function stack_main(): void {
