72338877746ba6430f012e74f88404675c3a72ad
tofr3054 <113582913+tofr3054@users.noreply.github.com>
2023-02-24 10:48:49 +0100

    added two empty pots

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 //main file
-import { holdem } from "./poker";
+import {holdem} from "./poker";
+import {Pot} from "./poker_types";
+import {make_pot} from "./stack_bet"
 
 function round() {
+    let pot1 = make_pot();
+    let pot2 = make_pot();
     return holdem(2);
 }
