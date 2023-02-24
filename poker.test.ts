86b7da544686f2d3d141d4245be7a4692309ed58
essz4367 <szantho.eszti@gmail.com>
2023-02-24 10:59:27 +0100

    pokerhands

--- poker.test.ts
+++ poker.test.ts
@@ -53,3 +53,31 @@ test('flush function is valid', () => {
 
     expect(flush(hand2).exists).toBe(false);
 });
+
+import {Pokerhand } from './poker_types'
+import {has_four_of_akind, has_fullhouse, has_pair, has_three_of_akind, has_two_pairs} from './poker_hands'
+
+const hand1: Hand = [{suit: 3, value: 3}, {suit: 0, value: 2}, {suit: 1, value: 3}, 
+    {suit: 1, value: 5}, {suit: 1, value: 2}, {suit: 3, value: 3}, {suit: 2, value: 3}];
+
+
+test('Hand has pair', () => {
+    const pair = has_pair(hand1).exists;
+    expect(pair).toStrictEqual(true);
+});
+test('Hand has pair returns correct value', () => {
+    expect(has_pair(hand1).value).toBe(2);
+});
+test('Hand has three of a kind', () => {
+    expect(has_three_of_akind(hand1).exists).toBe(true);
+});
+test('Hand has four of a kind', () => {
+    expect(has_four_of_akind(hand1).exists).toBe(true);
+});
+
+test('Hand has two pairs', () => {
+    expect(has_two_pairs(hand1).exists).toBe(true);
+});
+test('Hand has fullhouse', () => {
+    expect(has_fullhouse(hand1).exists).toBe(true);
+});
