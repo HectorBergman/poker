9ce4218190beb2e2d4a88db4de0987c830bc6668
essz4367 <szantho.eszti@gmail.com>
2023-02-24 14:53:07 +0100

    ..

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 import {holdem} from "./poker";
 import {Pot, GameState, Hands} from "./poker_types";
 import {make_pot, make_new_stack, show_game_state, add_pot} from "./stack_bet"
-import {}  from "./hand_rankings"
+import {}  from "./hands_ranking"
 
 function round(gs: GameState) {
     let pot1 = make_pot();
@@ -11,11 +11,7 @@ function round(gs: GameState) {
     if (hands == undefined) {
         add_pot(pot1, gs[1]);
         add_pot(pot2, gs[1])
-    } else {
-
-    }
-
-
+    } else {}
 }
 
 function stack_main(): void {
