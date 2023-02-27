ae055d6452f255ef3935efcf633ecde39d1bed9b
essz4367 <szantho.eszti@gmail.com>
2023-02-27 12:23:35 +0100

    ..

--- main.ts
+++ main.ts
@@ -4,6 +4,18 @@ import {Pot, GameState, Hands} from "./poker_types";
 import {make_pot, make_new_stack, show_game_state, add_pot} from "./stack_bet"
 import {}  from "./hands_ranking"
 
+function main(): void {
+    console.log("Poker");
+    console.log("");
+    const pn = 2;
+    const gs: GameState = [];
+    for (let i = 0; i < pn; i += 1) {
+        gs[i] = make_new_stack();
+    }
+    show_game_state(gs);
+    round(gs);
+}
+
 function round(gs: GameState) {
     let pot1 = make_pot();
     let pot2 = make_pot();
@@ -14,13 +26,3 @@ function round(gs: GameState) {
     } else {}
 }
 
-function stack_main(): void {
-    console.log("Poker");
-    console.log("");
-    const pn = 2;
-    const gs: GameState = [];
-    for (let i = 0; i < pn; i += 1) {
-        gs[i] = make_new_stack();
-    }
-    show_game_state(gs);
-}
