fec099e06ee3ff108976eed74c2e9f4ec83161df
essz4367 <szantho.eszti@gmail.com>
2023-02-27 14:30:36 +0100

    ..

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ var poker_1 = require("./poker");
 var stack_bet_1 = require("./stack_bet");
 var hands_ranking_1 = require("./hands_ranking");
 function round(gs) {
+    (0, stack_bet_1.show_game_state)(gs);
     var pot1 = (0, stack_bet_1.make_pot)();
     var pot2 = (0, stack_bet_1.make_pot)();
+    (0, stack_bet_1.min_wager)(gs[0], pot1);
+    (0, stack_bet_1.min_wager)(gs[1], pot2);
     var hands = (0, poker_1.holdem)(2, gs, pot1, pot2);
     if (hands == undefined) {
         (0, stack_bet_1.add_pot)(pot1, gs[1]);
@@ -15,16 +18,31 @@ function round(gs) {
     else {
         if ((0, hands_ranking_1.winners)(hands[0], hands[1]) === "Player 1 wins") {
             console.log("Player 1 wins");
+            (0, stack_bet_1.add_pot)(pot1, gs[0]);
+            (0, stack_bet_1.add_pot)(pot2, gs[0]);
+            if ((0, stack_bet_1.pot_value)(gs[1]) === 0) {
+                console.log("Player 1 wins the game");
+                poker_main();
+            }
         }
         else if ((0, hands_ranking_1.winners)(hands[0], hands[1]) === "Player 2 wins") {
             console.log("Player 2 wins");
+            (0, stack_bet_1.add_pot)(pot1, gs[1]);
+            (0, stack_bet_1.add_pot)(pot2, gs[1]);
+            if ((0, stack_bet_1.pot_value)(gs[1]) === 0) {
+                console.log("Player 1 wins the game");
+                poker_main();
+            }
         }
-        else {
+        else if ((0, hands_ranking_1.winners)(hands[0], hands[1]) === "It's a tie") {
             console.log("It's a tie");
+            (0, stack_bet_1.add_pot)(pot1, gs[0]);
+            (0, stack_bet_1.add_pot)(pot2, gs[1]);
         }
     }
+    round(gs);
 }
-function stack_main() {
+function poker_main() {
     console.log("Poker");
     console.log("");
     var pn = 2;
@@ -32,6 +50,6 @@ function stack_main() {
     for (var i = 0; i < pn; i += 1) {
         gs[i] = (0, stack_bet_1.make_new_stack)();
     }
-    (0, stack_bet_1.show_game_state)(gs);
+    round(gs);
 }
 round([(0, stack_bet_1.make_new_stack)(), (0, stack_bet_1.make_new_stack)()]);
