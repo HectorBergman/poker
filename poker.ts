84557305d9c391933a8fa2056ccb5da4a80e9661
tofr3054 <113582913+tofr3054@users.noreply.github.com>
2023-02-28 11:48:45 +0100

    .

--- poker.ts
+++ poker.ts
@@ -3,11 +3,7 @@ import {random_list, describe} from './helpers'
 import {Deck, Hand, Board, Pocketcards, Pokerhand, Card, Bet, Pot, Stack, Pile, GameState} from './poker_types'
 import {question} from 'readline-sync'
 import {displaycards} from './cardimages'
-<<<<<<< HEAD
 import {make_bet, call_bet, pot_value, make_pot} from './stack_bet';
-=======
-import {make_bet, call_bet, pot_value, make_pot, show_game_state} from './stack_bet';
->>>>>>> 5bd180887ecc4858f364bd6baee462ab2bc035f9
 
 /**
  * Generates a list of 52 unique cards with suit 0-3 and value 2-14 (11 = jack, 12 = queen, 13 = king, 14 = ace)
