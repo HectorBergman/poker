0b8dfe909a67629e9ed3c83b55a4402ad84993a7
tofr3054 <113582913+tofr3054@users.noreply.github.com>
2023-03-04 17:42:23 +0100

    Edited so strict compiling works

--- sorting.js
+++ sorting.js
@@ -3,12 +3,12 @@
 exports.__esModule = true;
 exports.sorter = void 0;
 function sorter(hand) {
-    function sortRecordsByValue(records, key) {
+    function sortRecordsByValue(records) {
         return records.sort(function (a, b) {
-            if (a[key] < b[key]) {
+            if (a.value < b.value) {
                 return -1;
             }
-            else if (a[key] > b[key]) {
+            else if (a.value > b.value) {
                 return 1;
             }
             else {
@@ -16,6 +16,8 @@ function sorter(hand) {
             }
         });
     }
-    return sortRecordsByValue(hand, 'value');
+    return sortRecordsByValue(hand);
 }
 exports.sorter = sorter;
+var hand3 = [{ suit: 2, value: 9 }, { suit: 3, value: 8 }, { suit: 3, value: 3 }, { suit: 0, value: 10 }, { suit: 0, value: 11 }, { suit: 3, value: 12 }, { suit: 3, value: 6 }];
+console.log(sorter(hand3));
--- sorting.ts
+++ sorting.ts
@@ -3,16 +3,20 @@
 import { Hand, Card } from "./poker_types";
 
 export function sorter(hand: Hand): Hand {
-  function sortRecordsByValue(records: Card[], key: string): Card[] {
+  function sortRecordsByValue(records: Card[]): Card[] {
     return records.sort((a, b) => {
-      if (a[key] < b[key]) {
+      if (a.value < b.value) {
         return -1;
-      } else if (a[key] > b[key]) {
+      } else if (a.value > b.value) {
         return 1;
       } else {
         return 0;
       }
     });
   }
-  return sortRecordsByValue(hand, 'value');
+  return sortRecordsByValue(hand);
 }
+
+const hand3 = [{suit: 2, value: 9}, {suit: 3, value: 8}, {suit: 3, value: 3}, {suit: 0, value: 10}, {suit: 0, value: 11}, {suit: 3, value: 12}, {suit: 3, value: 6}];
+
+console.log(sorter(hand3));
