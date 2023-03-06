f52662b0706458910718023659acb614e87b9fcf
tofr3054 <113582913+tofr3054@users.noreply.github.com>
2023-03-06 11:34:26 +0100

    Removed console log

--- sorting.ts
+++ sorting.ts
@@ -21,7 +21,3 @@ export function sorter(hand: Hand): Hand {
   }
   return sortRecordsByValue(hand);
 }
-
-const hand3 = [{suit: 2, value: 9}, {suit: 3, value: 8}, {suit: 3, value: 3}, {suit: 0, value: 10}, {suit: 0, value: 11}, {suit: 3, value: 12}, {suit: 3, value: 6}];
-
-console.log(sorter(hand3));
